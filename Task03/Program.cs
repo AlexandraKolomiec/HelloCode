380aa713a2d9b3e22e23a00d644371299d02a382
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-20 22:44:30 +0300

    надо чтоб получилось уже

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 22;
+int numberB = 11;
+if (numberA > numberB)
+{
+   Console.Write(" большее число = ");
+   Console.Write(numberA);
+   Console.Write(" меньшее число = ");
+   Console.Write(numberB);
+}
+else
+{
+    Console.Write(" большее число = ");
+    Console.Write(numberB);
+    Console.Write(" меньшее число = ");
+    Console.WriteLine(numberA);
+} 
