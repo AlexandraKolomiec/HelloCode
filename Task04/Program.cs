1bfa712f8e180f07ef7ae00ef64daa18a912f12b
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-20 23:04:59 +0300

    add task4

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("введите число от 1 до 7");
+string number = Console.ReadLine();
+if(number == 1)
+{
+    Console.WriteLine("Понедельник")
+}
+
+
+
