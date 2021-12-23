4f79c7daeb4d377ecf7705f6baf02609a8dde2eb
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-23 22:00:56 +0300

    четность числа

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿// является ли число четным выяснить
+int a;
+Console.WriteLine("введите число");
+a = Console.Read();
+if(a%2 == 1)
+{
+    Console.WriteLine("число нечетное");
+}
+else
+{
+    Console.WriteLine("число четное");
+}
