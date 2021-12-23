9a616eb114e06673f0f9c8228cbf742668d86871
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-23 21:13:37 +0300

    max number

---
+++ Task0004/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 2;
+int numberB = 8;
+int numberC = 6;
+int max = numberA;
+if(numberB > max)
+{
+   max = numberB;
+}
+if (numberC > max)
+{
+  max = numberC;
+}
+Console.WriteLine(max);
