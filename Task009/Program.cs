cfedbc4f250c5887305e64c5e4ee3bc3a84a6985
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-29 21:36:41 +0300

    нахождение 1,2,3 цифры числа

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -1,6 +1,18 @@
 ﻿// показать 3 цифру трехзначного числа
 
 int n = 128;
-int last = (n%10);
+
+int third = (n%10);
 Console.Write("последняя цифра ");
-Console.WriteLine(last);
+Console.WriteLine(third);
+
+// показать 2 цифру трехзначного число
+int second = ((n/10)%10);
+Console.Write("вторая цифра ");
+Console.WriteLine(second);
+
+// показать 1 цифру трехзначного числа, 
+//можно не искать отаток от деления %10, если число трехзначное n/100
+int first = ((n/100)%10);
+Console.Write("первая цифра ");
+Console.WriteLine(firs);
