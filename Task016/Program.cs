d9b9cf4991e8dcde9a61d222067e82e9b7e4c948
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-30 21:13:36 +0300

    определить является ли день недели выходным

---
+++ Task016/Program.cs
@@ -0,0 +1,15 @@
+﻿// является ли номер дня недели выходным
+Console.Write("введите число от 1 до 7 ");
+string dayOfWeek = Console.ReadLine();
+if (dayOfWeek == "6")
+{
+    Console.WriteLine("ура, выходной!");
+}
+else if (dayOfWeek == "7")
+{
+    Console.WriteLine("ура, выходной!");
+}
+else
+{
+    Console.WriteLine("скорее бы выходной");
+}
