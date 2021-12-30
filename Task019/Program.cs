43543098b7f254de8fe529aea5a083735c38d511
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-30 22:00:20 +0300

    четверти плоскости по координатам точки

---
+++ Task019/Program.cs
@@ -0,0 +1,23 @@
+﻿// номер четверти плоскости для точки с координатами
+int x = 0;
+int y = 0;
+if (x > 0 && y > 0)
+{
+   Console.WriteLine(" 1 четверть"); 
+}
+else if (x < 0 && y > 0)
+{
+   Console.WriteLine(" 2 четверть"); 
+}
+else if (x < 0 && y < 0)
+{
+   Console.WriteLine(" 3 четверть"); 
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine(" 4 четверть"); 
+}
+else
+{
+    Console.WriteLine("ОШИБКА");
+}
