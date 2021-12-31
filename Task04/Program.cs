3b5f740119e963bddf323db4043f933ab9d247de
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-31 18:26:23 +0300

    по заданному номеру вывести день недели

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,10 +1,31 @@
 ﻿// По заданному номеру дня недели вывести его название
 Console.WriteLine("введите число от 1 до 7");
-string number = Console.ReadLine();
-if(number == 1)
+string numb = Console.ReadLine();
+if(numb == "1")
 {
-    Console.WriteLine("Понедельник")
+    Console.WriteLine("Понедельник");
 }
-
-
-
+if(numb == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if(numb == "3")
+{
+    Console.WriteLine("Среда");
+}
+if(numb == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if(numb == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if(numb == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if(numb == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
