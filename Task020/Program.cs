c11a79cfdb66c2c44cfd548e65dc5382d4c119f1
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-31 18:43:24 +0300

    по номеру четверти указать диапазон координат

---
+++ Task020/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("введите номер четверти от 1 до 4");
+string quarter = Console.ReadLine();
+if (quarter == "1")
+{
+    Console.Write(" диапазоны возможных координат в пределах:");
+    Console.WriteLine(" x > 0 , y > 0");
+}
+if (quarter == "2")
+{
+    Console.Write(" диапазоны возможных координат в пределах:");
+    Console.WriteLine(" x < 0 , y > 0");
+}
+if (quarter == "3")
+{
+    Console.Write(" диапазоны возможных координат в пределах:");
+    Console.WriteLine(" x < 0 , y < 0");
+}
+if (quarter == "4")
+{
+    Console.Write(" диапазоны возможных координат в пределах:");
+    Console.WriteLine(" x > 0 , y < 0");
+}
