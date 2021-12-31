37599cac05598ba10bc89048abc413483ae1664f
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-31 19:37:17 +0300

    расстояние между точками 2Д, 3Д

---
+++ Task022/Program.cs
@@ -0,0 +1,13 @@
+﻿// расстояние между точками в 2D и 3D
+int xA = 2;
+int xB = 4;
+int yA = 3;
+int yB = 6;
+int zA = 5;
+int zB = 10;
+
+Console.Write("расстояние между точками 2D= ");
+Console.WriteLine(Math.Sqrt((Math.Pow(xB - xA , 2) +  Math.Pow(yB - yA , 2))));
+
+Console.Write("расстояние между точками 3D= ");
+Console.WriteLine(Math.Sqrt((Math.Pow(xB - xA , 2) +  Math.Pow(yB - yA , 2) + Math.Pow(zB - zA , 2))));
