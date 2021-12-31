97d07dd9032df28cc35ec9398684bccbbfc8107e
Cheshire Cat <Sasha-Km@yandex.ru>
2021-12-31 19:00:02 +0300

    палиндром

---
+++ Task021/Program.cs
@@ -0,0 +1,10 @@
+﻿// палиндром из 5 цифр
+int[] array = { 1, 2, 3, 2, 1};
+if (array[0] == array [4] && array[1] == array [3])
+{
+    Console.WriteLine("это палиндром");
+}
+else
+{
+    Console.WriteLine(" не палиндром ");
+}
