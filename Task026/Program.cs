b96357542339c02f6d2923b979e59352fee91e02
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-13 22:18:41 +0300

    Возвести число А в степень В при помощи цикла

---
+++ Task026/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возвести число А в степень В при помощи цикла
+
+Console.Clear();
+Console.Write("Введите число, которое нужно вознести в степень: "); 
+double chislo = Convert.ToDouble(Console.ReadLine());
+double vozvedenie = 1;
+Console.Write("Введите степень: ");
+int stepen = Convert.ToInt32(Console.ReadLine()); 
+for (int i = 0; i < stepen; i++)
+ {    
+     vozvedenie *= chislo;
+ } 
+Console.Write("Ответ: ");
+Console.Write(vozvedenie);
