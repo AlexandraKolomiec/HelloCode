c31a7426b31942e91ad4d6239692f49586c0dca9
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-13 22:06:57 +0300

    сумма цифр в числе

---
+++ Task028/Program.cs
@@ -0,0 +1,29 @@
+﻿// сумма цифр в числе
+//Console.Clear();
+//Console.Write("Введите число: ");
+//int number=int.Parse(Console.ReadLine());
+//int i=0;
+//while(number>0) 
+{
+  // i = i + number % 10;
+  // number /= 10;
+}
+//Console.WriteLine("Сумма числа: {0}", i);
+//return 0;
+
+int SumNumbers (int A)
+{
+    int sum = 0;
+    while (A > 0)
+    {
+        int B = A % 10;
+        A = A/10;
+        sum = sum + B;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int N = SumNumbers (A);
+Console.WriteLine($"В числе {A} сумма цифр равна {N}");
