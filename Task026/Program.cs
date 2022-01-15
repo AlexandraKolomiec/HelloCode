b56f5bc1961df4f3cb79bc5183dc8dfc083b408f
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-15 21:11:30 +0300

    число в степень цикл

--- Task026/Program.cs
+++ Task026/Program.cs
@@ -1,14 +1,31 @@
 ﻿// Возвести число А в степень В при помощи цикла
 
-Console.Clear();
-Console.Write("Введите число, которое нужно вознести в степень: "); 
-double chislo = Convert.ToDouble(Console.ReadLine());
-double vozvedenie = 1;
-Console.Write("Введите степень: ");
-int stepen = Convert.ToInt32(Console.ReadLine()); 
-for (int i = 0; i < stepen; i++)
+//Console.Clear();
+//Console.Write("Введите число, которое нужно вознести в степень: "); 
+//double chislo = Convert.ToDouble(Console.ReadLine());
+//double vozvedenie = 1;
+//Console.Write("Введите степень: ");
+//int stepen = Convert.ToInt32(Console.ReadLine()); 
+//for (int i = 0; i < stepen; i++)
  {    
-     vozvedenie *= chislo;
+  //   vozvedenie *= chislo;
  } 
-Console.Write("Ответ: ");
-Console.Write(vozvedenie);
+//Console.Write("Ответ: ");
+//Console.Write(vozvedenie);
+
+int degree(int A, int B)
+ {  
+    int count = 1;  
+    int degree = 1; 
+    while (count <= B)  
+    {   degree = degree*A;  
+        count++;   
+    }    
+    return degree;
+ } 
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень возвести?");
+int B = Convert.ToInt32(Console.ReadLine());
+int N = degree(A, B);
+Console.WriteLine(N);
