c9573ca728fed367485c3b54ab91783767c58d40
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-19 21:57:05 +0300

    сумма чисел одномерного массива стояших на нечетной позиции

---
+++ Task038/Program.cs
@@ -0,0 +1,22 @@
+﻿// сумма чисел одномерного массива стояших на нечетной позиции
+int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,20);
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine();
+
+int SumOddPositionArray (int [] array)
+{
+    int SumOddPosition = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        //if (array [i] % 2 == 1) SumOddPosition = SumOddPosition + array [i]; сумма нечетных значений
+        if (i % 2 == 1) SumOddPosition = SumOddPosition + array [i];
+    }
+    return SumOddPosition;
+}
+
+int N = SumOddPositionArray (array);
+Console.WriteLine($" Сумма элементов стоящих на нечетной позиции равна {N}");
