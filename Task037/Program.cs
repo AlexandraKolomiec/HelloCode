515c098b8bf94d88d312831e3f13331c5e46794b
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-19 21:58:52 +0300

    в одномерном массиве чисел найти кол-во эл-тов из отрезка

---
+++ Task037/Program.cs
@@ -0,0 +1,10 @@
+﻿// в одномерном массиве из 123 чисел нати кол-во эл-тов из отрезка [10, 99]
+int[] array = new int[123];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+
+}
+//Console.WriteLine(array[99] - array[10]);
+Console.WriteLine((array.Length - 24) - (array.Length - 113)); // 123-99=24, 123-10=113
+    
