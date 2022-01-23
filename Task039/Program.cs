71ed76b1770e85535633d1d038e7e4656c6bbebb
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-23 20:02:31 +0300

    произведение пар чисел в одномерном массива. Пара 1 и последний, 2 и предпоследний итд

---
+++ Task039/Program.cs
@@ -0,0 +1,22 @@
+﻿// произведение пар чисел в одномерном массива. Пара 1 и последний, 2 и предпоследний итд
+
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,25);
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine();
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    int Multiply = array[i] * array[(array.Length - i - 1)];
+    Console.WriteLine (Multiply);
+    if (i >= array.Length / 2 -1) break;
+}
+
+
+
+
+     
+ 
---
+++ Task040/Program.cs
@@ -0,0 +1,56 @@
+﻿// в массиве вещественных чисел найти разницу между min max эл-тами
+
+//ouble [] array = new double[10];
+//for (double i = 1; i < array.Length; i++)
+{
+  //array[i] = new Random.NextDouble(); //* 10 + 10; // диапазон[10,20]
+  //Console.WriteLine(array[i]);
+}
+//double max = array[0];
+//double min = array[0];
+//for (int i = 0; i < Array.Length; i++)
+{
+  //if (array[i] = max)
+    {
+     //Console.WriteLine(max);
+    }
+ 
+  //else if (array[i] = min)
+    {
+      //Console.WriteLine(min);  
+    }
+    
+  //else (i++);
+}  
+
+//Console.WriteLine(max - min);
+
+// в массиве вещественных чисел найти разницу между min max эл-тами
+double[] Array = { 4, 27, 18, 10, 12, 14, 16, };
+
+double MaxNumber(double[] Array)
+{
+  double max = Array[0];
+  for (int i = 0; i < Array.Length; i++)
+  {
+    if (Array[i] > max) max = Array[i];
+  }
+  return max;
+}
+
+double MinNumber(double[] Array)
+{
+  double min = Array[0];
+  for (int i = 0; i < Array.Length; i++)
+  {
+    if (Array[i] < min) min = Array[i];
+  }
+  return min;
+}
+
+double a = MaxNumber(Array); 
+System.Console.WriteLine(a);
+double b = MinNumber(Array); 
+System.Console.WriteLine(b);
+System.Console.WriteLine(a-b);
+
