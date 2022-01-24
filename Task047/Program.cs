a30ad7dabc3528d38605466d285a1ec5bb015b88
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-24 22:00:58 +0300

    копирование массива

---
+++ Task047/Program.cs
@@ -0,0 +1,10 @@
+﻿// копирование массива
+
+int [] array = new int [6] {8, -3, 16, -6, -1, 0}; 
+int[] copy = new int [6];
+
+for(int i = 0; i < 6; i++)
+{
+    copy[i] = array[i];
+    Console.WriteLine (copy[i]);
+}
