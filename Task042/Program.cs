6d0039a78b7a9e1393fa8c42a1e6d78e086d0a4e
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-25 20:45:29 +0300

    Определить сколько чисел больше 0 введено с клавиатуры

---
+++ Task042/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+int i = 0;
+int count = 0;
+while (i < 5)
+{
+    Console.WriteLine("введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if(a > 0)
+    {
+        count = count + 1; 
+    }
+    i++;
+}
+Console.WriteLine($"количество чисел больше 0 = {count}");
+
