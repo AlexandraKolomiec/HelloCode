7d237225def5eb2c26f7a3dc3ebbc83f46f868ea
Cheshire Cat <Sasha-Km@yandex.ru>
2022-01-25 20:40:12 +0300

    преобразование десятичного числа в двоичное

---
+++ Task043/Program.cs
@@ -0,0 +1,49 @@
+﻿//преобразование десятичного числа в двоичное
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string binary = Convert.ToString(number, 2);
+Console.WriteLine(binary);
+
+
+
+ // 43. Написать программу преобразования десятичного числа в двоичное
+//Console.WriteLine("Ведите десятичное число для преобразования его в двоичное");
+//int Number = Convert.ToInt32(Console.ReadLine());
+
+//int[] ConvertToBinary(int decimalnumber)
+{
+    //int counter = 0;
+    //int howLongArr = decimalnumber;
+    //while (howLongArr >= 1)
+    {
+       // howLongArr = howLongArr / 2;
+       // counter++;
+    }
+    //int[] arrayBinaryNumber = new int[counter];
+    //int dividend = decimalnumber;
+    //int fromBackCounter = arrayBinaryNumber.Length - 1;
+    //while (dividend >= 1)
+    {
+        //arrayBinaryNumber[fromBackCounter] = dividend % 2;
+        //dividend = dividend / 2;
+       //--fromBackCounter;
+    }
+//return arrayBinaryNumber;
+}
+//void PrintArray (int[] arraytoprint)
+{
+    //for (int i = 0; i < arraytoprint.Length; i++)
+    {
+        //Console.Write(arraytoprint[i]);
+    }
+//Console.WriteLine();
+}
+// ConvertToBinary(Number);
+//PrintArray(ConvertToBinary(Number));
+//nt[] binaryArray = ConvertToBinary(Number);
+//PrintArray(binaryArray);
+
+
+
+
+
