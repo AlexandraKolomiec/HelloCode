edc0afcf8c81265f220d8a75ff9f41b294fbc43d
Cheshire Cat <Sasha-Km@yandex.ru>
2022-02-01 13:03:17 +0300

    показать натуральные числа от М до N, M и N заданы, рекурсия

---
+++ Task068/Program.cs
@@ -0,0 +1,13 @@
+﻿//показать натуральные числа от М до N, M и N заданы
+void PrintArray(int n, int i)
+{
+    Console.Write($"{i} ");
+    if (i != n)
+        PrintArray(n, i + 1);
+}
+
+Console.WriteLine("Введите первое число ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+PrintArray(N, M);
