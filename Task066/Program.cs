4c51e2a5465e494b3721956588272345b7b2755e
Cheshire Cat <Sasha-Km@yandex.ru>
2022-02-01 12:57:23 +0300

    Рекурсия, показать натуральные числа от 1 до N, N задано

---
+++ Task066/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия, показать натуральные числа от 1 до N, N задано
+
+void PrintN(int n, int i =1)
+{
+    Console.Write($"{i} ");
+    if (i != n)
+        PrintN(n, i + 1);
+}
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintN(N);
+
