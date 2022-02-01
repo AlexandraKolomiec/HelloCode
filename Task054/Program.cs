376cc9f66ea6c363e65cbfabd4bae160eadba45e
Cheshire Cat <Sasha-Km@yandex.ru>
2022-02-01 13:38:27 +0300

    Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов

---
+++ Task054/Program.cs
@@ -0,0 +1,24 @@
+﻿// в матрице чисел найти сумму элементов главной диагонали
+int[,] matrix = new int[6, 6];
+
+void PrintFillArray(int[,] mat)
+{
+  for (int i = 0; i < mat.GetLength(0); i++)
+  {
+    for (int j = 0; j < mat.GetLength(1); j++)
+    {
+      mat[i, j] = new Random().Next(1, 10);
+      System.Console.Write($"{mat[i, j]} ");
+    }
+    System.Console.WriteLine();
+  }
+}
+PrintFillArray(matrix);
+Console.WriteLine();
+
+int SumOfDiagonal = 0;
+for (int i = 0; i < matrix.GetLength(0); ++i)
+{   
+  SumOfDiagonal = SumOfDiagonal + matrix[i, i];
+}
+Console.WriteLine($"сумма элементов главной диагонали = {SumOfDiagonal} ");
---
+++ Task055/Program.cs
@@ -0,0 +1,38 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+int m = 3; //число строк
+int n = 4; // число столбцов
+int [,] matrix = new int [m,n];
+
+void Matrix (int [,] matrix)
+{
+    for (int i=0; i< matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+                matrix [i,j] = new Random().Next(1,10);
+                Console.Write(matrix[i,j]);
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void averageAtColumns (int [,] matrix)
+{
+    int sum = 0;
+    for (int j=0; j< matrix.GetLength(1);j++)
+    {
+        for (int i=0; i<matrix.GetLength(0);i++)
+        {
+            sum = sum+matrix [i,j]; // суммируем элементы первого столбца
+        }
+        Console.Write($"{sum/m} "); // делим сумму на количнство строк
+        sum = 0; // обнуляем сумму элементов первого столбца
+    }
+    
+}
+Matrix (matrix);
+Console.WriteLine("Average:");
+averageAtColumns (matrix);
+Console.WriteLine();
